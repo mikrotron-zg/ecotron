1c73022bb183b01179569359ed71f86f7528e8e1
Tomislav Preksavec <tomislav.preksavec@gmail.com>
2016-08-17 15:28:01 +0200

    Implemented measurement display as percentage in 10 percent steps

--- webapp/src/main/java/org/geomajas/quickstart/gwt2/client/DemoDialogBox.java
+++ webapp/src/main/java/org/geomajas/quickstart/gwt2/client/DemoDialogBox.java
@@ -50,21 +50,21 @@ public class DemoDialogBox extends DialogBox implements ClickHandler {
 
     public void write(String field, String val) {
     	if ( "can1".equals(field)) {
-    		can1.setInnerText(val);
+    		can1.setInnerText(percentage(val));
     	} else if ( "can2".equals(field)) {
-    		can2.setInnerText(val);
+    		can2.setInnerText(percentage(val));
     	} else if ( "can3".equals(field)) {
-    		can3.setInnerText(val);
+    		can3.setInnerText(percentage(val));
     	} else if ( "can4".equals(field)) {
-    		can4.setInnerText(val);
+    		can4.setInnerText(percentage(val));
     	} else if ( "can5".equals(field)) {
-    		can5.setInnerText(val);
+    		can5.setInnerText(percentage(val));
     	} else if ( "temp".equals(field)) {
-    		temp.setInnerText(val);
+    		temp.setInnerText(val + " °C");
     	} else if ( "gpsLatitude".equals(field)) {
-    		gpsLatitude.setInnerText(val);
+    		gpsLatitude.setInnerText(val + "°");
     	} else if ( "gpsLongitude".equals(field)) {
-    		gpsLongitude.setInnerText(val);
+    		gpsLongitude.setInnerText(val + "°");
     	} else if ( "gpsTime".equals(field)) {
     		gpsTime.setInnerText(val);
     	} else if ( "time".equals(field)) {
@@ -73,6 +73,23 @@ public class DemoDialogBox extends DialogBox implements ClickHandler {
     		stationId.setInnerText(val);
     	}
     }
+    
+    private String percentage(String val){
+    	
+    	//FIXME exact lengths will be known after sensors are mounted
+    	final double empty = 120;
+    	final double full = 40;
+    	double measured = Double.parseDouble(val);
+        
+    	if (measured <= (full+5)) return "100 %";
+    	if (measured >= (empty-5)) return "0 %";
+    	
+    	double calc = (1 - ((measured-full)/(empty-full)) + 0.05)*10;
+    	int rounded = ((int)calc*100)/10;
+    	
+    	return rounded + " %";
+    }
+    
 	@Override
 	public void onClick(ClickEvent event) {
 		hide();
