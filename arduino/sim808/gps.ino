2576fd8ea0c6a6aa6b3b974ea480bd4580f5a17d
Josip Almasi <joe@vrspace.org>
2017-02-25 17:38:29 +0100

    GPS timeout parametrized

--- arduino/sim808/gps.ino
+++ arduino/sim808/gps.ino
@@ -1,15 +1,20 @@
+// number and timeout for GPS to fix (lock)
+#define MAX_GPS_TRIES 30
+#define GPS_RETRY_DELAY 1000
+
 void getGPSinfo(){
   // full procedure for turning gps on, getting data and turning it off again
   debugPrint(F("GPS on "));
   if(!checkResp(F("at+cgnspwr=1"),20,1000,ok))gpsdata="err";
   debugPrint(F("GPS INFO "));
   String ret="0";
-  // TODO: introduce safety timeout here, or GPS may get into endless loop
+  // safety count prevents GPS getting into endless loop when unreliable
+  int gpstries = 0;
   do{
+    delay(GPS_RETRY_DELAY);
     gpsdata=parseinfo();
     debugPrintln("bot "+ret);
-    delay(500);
-  }while(gpsdata[2]!='1');
+  }while(gpsdata[2]!='1' && ++gpstries < MAX_GPS_TRIES);
   debugPrint(F("GPS off "));
   if(!checkResp(F("at+cgnspwr=1"),20,1000,ok))debugPrintln(F("GPS PWR ERR"));
 }
